19c750a668b4cc5ae4c484a7936e81d20393ebaa
Brian Ryckbost <bryckbost@gmail.com>
2012-01-06 16:10:22 -0500

    Toggle the blurred state of the body

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.insertCSS(null, {code: ".squint-blurred { -webkit-filter: blur(3px); }"});
+
   chrome.tabs.executeScript(null, 
     {
-      code: "document.body.style.setProperty('-webkit-filter', 'blur(1px)')"
+      code: "if (document.body.className.match(/squint-blurred/)) {document.body.className -= ' squint-blurred';} else { document.body.className += ' squint-blurred';}"
     }
   );
 });
